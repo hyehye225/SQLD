7418d9d0f95ebf5fd2cc56b024341cdcdf488f98
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-08-24 08:35:53 +0900

    DML

---
+++ DML/DML_1.sql
@@ -0,0 +1,6 @@
+select * from dual;
+select * from player;
+select player_id, player_name,nickname from player;
+select distinct position from player;
+select team_id, position from player;
+select distinct team_id, position from player;
